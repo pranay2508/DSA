f4b078d8a563ef3508ef667a8fc694c425615b05
Pranay bhatnagar <bhatnagarpranay8@gmail.com>
2024-05-15 23:21:47 +0530

    first commit

---
+++ Intro c++/first.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+// using std :: cout ;
+// int main() {
+//     int pranay = 8;
+//     cout << pranay ;
+// };
+
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int a = 11 ;
+    int b = ++a * ++a;
+    cout << b;
+
+    return 0;
+}
