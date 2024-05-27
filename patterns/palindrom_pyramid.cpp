1771ac6224a1eda8b7669d119566d3c19464eed0
Pranay bhatnagar <bhatnagarpranay8@gmail.com>
2024-05-28 03:24:41 +0530

    patten

--- patterns/palindrom_pyramid.cpp
+++ patterns/palindrom_pyramid.cpp
@@ -1,31 +1,54 @@
-#include <iostream>
-using namespace std ;
-int main (){
-    int n ;
-    cin >> n;
-    int k =n ;
-    for(int i=0;i<n;i++){
-        int c=1;
-        for(int j=0;j<k;j++){
-            if(j<n-i-1){
-                cout <<" ";
-            }
-            else if(j<=n-1){
-                cout <<c;
-                c++;
-            }
-            else if (j==n){
-                c=c-2;
-                cout <<c;
-                c--;
-            }
-            else {
-                cout<< c;
-                c--;
-            }
-        }
-        k++;
-        cout<<endl;
-    }
-    return 0;
-}
+// #include <iostream>
+// using namespace std ;
+// int main (){
+//     int n ;
+//     cin >> n;
+//     int k =n ;
+//     for(int i=0;i<n;i++){
+//         int c=1;
+//         for(int j=0;j<k;j++){
+//             if(j<n-i-1){
+//                 cout <<" ";
+//             }
+//             else if(j<=n-1){
+//                 cout <<c;
+//                 c++;
+//             }
+//             else if (j==n){
+//                 c=c-2;
+//                 cout <<c;
+//                 c--;
+//             }
+//             else {
+//                 cout<< c;
+//                 c--;
+//             }
+//         }
+//         k++;
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+// half pyramid solid 
+
+// #include <iostream>
+// using namespace std ;
+// int main (){
+//     int n;
+//     cin >> n;
+//     for(int i=0  ; i < n ; i++){
+//         for (int j = 0 ; j<=i;j++){
+//             cout<<"*";
+//         }
+//         cout<< endl;
+//     }
+//     for (int i=1; i <=n-1;i++){
+//         for(int j=0;j<n-i;j++){
+//             cout << "*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
